f93cdf3b73af37d297ff71270e33b129ab80b948
pahuni232004 <pahu567@gmail.com>
2026-01-08 00:12:50 +0530

    submissions closed

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,10 @@ import MeetCrew from "./components/Meetcrew";
 import Navbar from "./components/Navbar";
 import DogScroller from "./components/ScrollScene";
 import Awards from "./components/Awards";
-import Popups from "./components/Popups";
-
 
 export default function Page() {
   return (
     <main className="overflow-x-hidden">
-      <Popups />
       <DogScroller/>
       <MeetCrew/>
       <Awards/>
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -39,3 +39,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
 
 
+
+
