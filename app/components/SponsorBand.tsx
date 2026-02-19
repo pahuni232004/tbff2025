a7c1729d8423140701f209f053ce802bc2276d17
pahuni232004 <pahu567@gmail.com>
2026-02-19 19:51:44 +0530

    fixed sponsor band in mobile view

--- app/components/SponsorBand.tsx
+++ app/components/SponsorBand.tsx
@@ -2,7 +2,7 @@
 
 export default function SponsorBand() {
     return (
-        <div className="w-full relative z-50 -mt-[150px] md:-mt-[200px] -translate-y-20 md:-translate-y-32">
+        <div className="w-full relative z-50 -mt-[150px] md:-mt-[200px] translate-y-6 md:-translate-y-8 mb-10 md:mb-4">
             <img
                 src="/assets/sponsor band.png"
                 alt="Our Sponsors"
